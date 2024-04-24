0c5995a95045792393cfa716adb6e499b3de5f48
mhranik <mhranik@ipa.gpext.local>
2024-04-24 14:05:51 +0300

    added deploy

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,14 @@ const exec = require("@actions/exec");
 const github = require("@actions/github");
 
 function run() {
+
+    const bucket = core.getInput("bucket", { required: true });
+    const bucketRegion = core.getInput("bucket-region", { required: true });
+    const sourceCode = core.getInput("source-code", { required: true });
+
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${sourceCode} ${s3Uri} --region ${bucketRegion}`)
+
     core.notice("Hello from my custom JavaScript action!");
 }
 
